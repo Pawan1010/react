037b98d662e757641c9c0dbab920f375169ea1fd
Pawan1010 <yadavpawan412@gmail.com>
2023-10-19 20:58:00 +0530

    api calling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      name : {firstName: "Pawan", lastName: "Yadav"},
-      company : "Vipul Organics"
-
-    }
+      monsters : []
+  }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(
+      () => {
+              return {monsters : users}
+            },
+      () => {
+              console.log(this.state)
+            }
+    ))
   }
   render() {
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi I am {this.state.name.firstName} {this.state.name.lastName}, 
-            I work in {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              () => {
-              return {                                            //this function is state change
-                name : {firstName: "Krishna", lastName: "Ram"}
-              }
-            },
-              () => {
-                console.log(this.state)
-              }
-            );
-            }}>Change Name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster)=> {
+          return<div key={monster.id}>
+                  <h1>{monster.name}</h1>
+                </div>})
+        }
       </div>
     );
   }
